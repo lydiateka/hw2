5405e764409ad6680433ec263221f8c2b68bd90b
Lydia Teka <teka@usc.edu>
2025-10-08 04:07:28 -0700

    removed hw 2 tests

--- hw2_tests/testing_utils/kwsys/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/csci104/material/artifacts/hw2/grading/amazon/testing_utils/kwsys
-# Build directory: /work/csci104/material/artifacts/hw2/grading/amazon/testing_utils/kwsys
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
